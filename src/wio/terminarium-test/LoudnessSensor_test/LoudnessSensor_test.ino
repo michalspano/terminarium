a93bca785ed4322a5172bf0af04f38aae1c95d6e
gusbomjod <gusbomjod@student.gu.se>
2023-04-19 12:42:27 +0200

    Parse data from loudness sensor
    This commit parses the loudness data from sensor and converts to percentage of max possible reading

--- src/wio/terminarium-test/LoudnessSensor_test/LoudnessSensor_test.ino
+++ src/wio/terminarium-test/LoudnessSensor_test/LoudnessSensor_test.ino
@@ -1,10 +1,13 @@
 #define LOUDNESS_PIN 0
-
+#define MAX_READING 1023 //Max possible reading from sensor
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  int loudness = analogRead(LOUDNESS_PIN);
+  int loudness = analogRead(LOUDNESS_PIN); //Reading sensor value
+
+  float loudnessPercent = ((float) loudness / (float) MAX_READING) * 100.0; // Calculating loudness as percentage of highest possible reading
+
   if(loudness != NULL){
       Serial.println("Signal received");
   }else{
