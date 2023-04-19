090c0c5c3cf6f627891f7f242ab2b33526acca61
Erik <elindstr@student.chalmers.se>
2023-04-19 13:09:39 +0200

    Refactor code comments based on merge-request feedback.
    This commit adds code comments to clarify the sensor setup as requested.

--- src/wio/terminarium-test/TempHumiSensor_test/TempHumiSensor_test.ino
+++ src/wio/terminarium-test/TempHumiSensor_test/TempHumiSensor_test.ino
@@ -1,24 +1,26 @@
 //import DHT library
 #include "DHT.h"
 
-//define pin of sensor
+//define pin of sensor (temp&humi)
 #define DHTPIN D0  
 
-//define type of DHT sensor
+//define type of DHT sensor (temp&humi)
 #define DHTTYPE DHT11
 
-//initialise sensor
+//initialise sensor-struct (temp&humi)
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+
+  //initialise sensor (temp&humi)
   dht.begin();
 }
 
 void loop() {
   //store temperature & humidity data in variables
-  float temp  = dht.readTemperature();
-  int humi    = dht.readHumidity();
+  float temp    = dht.readTemperature();
+  int humi      = dht.readHumidity();
   
   //print temperature in Celcius
   Serial.printf("Temperature: %.2f°C\n", temp);
