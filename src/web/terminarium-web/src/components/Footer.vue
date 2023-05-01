1f4fa12e0f9a4614f61547c30a5e4385db5a3ce7
manely <manely@student.chalmers.se>
2023-05-01 23:32:38 +0200

    Create basic footer file and import it in App.vue
    This commit creates the basic footer file (still to be worked on), imports it to the App.vue file and adds it to the template section

---
+++ src/web/terminarium-web/src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer>
+        <div class="terminarium_footer">
+            <h1>Terrminarium</h1>
+        </div>
+        <div class="footer-bottom">
+            <p> &copy;2023 Copyright: <Terrminarium class="com"></Terrminarium></p>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer.vue'
+}
+</script>
+
+<style scoped>                
+
+* {
+  outline: red dashed 1px;    
+}
+
+.terrminarium_footer {
+  background-color: #5A5A5A;
+  color: #fff;
+  display: flex;              
+  align-items: center;
+  height: 100px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 20px;
+}
+</style>
