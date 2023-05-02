628de45de06f6848b7d7c94d5d3038341f0c42f9
manely <manely@student.chalmers.se>
2023-05-02 22:29:58 +0200

    #39 Add background component
    This commit adds background component and integrates it into the App.vue test file. It also adds the background jpg in the assets folder

---
+++ src/web/terminarium-web/src/components/Background.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="background-image"></div>
+  </template>
+  
+  <style>
+  .background-image {
+    background-image: url('../assets/Background.jpg');
+    background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  }
+  </style>
+  
+  <script>
+  export default {
+    name: 'Background'
+  }
+  </script>
