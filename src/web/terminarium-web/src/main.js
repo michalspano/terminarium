c1982a7e474828d29009a887995c57b9eb8c0c16
manely <manely@student.chalmers.se>
2023-05-06 16:07:34 +0200

    #57 Fix merge conflict
    This commit fixes the merge conflict with main

--- src/web/terminarium-web/src/main.js
+++ src/web/terminarium-web/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VuePlyr from 'vue-plyr'
+import 'vue-plyr/dist/vue-plyr.css'
+import "@fontsource/cantora-one"
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(VuePlyr, {
+        plyr: {}
+    })
+    
+    .mount('#app')
+
+    
