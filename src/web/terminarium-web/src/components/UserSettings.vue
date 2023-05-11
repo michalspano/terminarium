a627e0af78f69079788833e40c1e5c6be70cd9ad
spano <spano@student.chalmers.se>
2023-05-11 12:24:43 +0200

    #58 Apply additional refactoring to UserSettings component
    This commit fixes the expressed concerns with the styling of the UserSettings component, as well as applies additional refactoring to the component. Co-authored-by: Manely Abbasi manely@student.chalmers.se

--- src/web/terminarium-web/src/components/UserSettings.vue
+++ src/web/terminarium-web/src/components/UserSettings.vue
@@ -1,142 +1,125 @@
 <template>
+  <!-- Container for the form -->
   <div class="form-entries">
+    <!-- 1st row: name and avatar -->
     <div class="row-name">
-      <img class="avatar" src="../assets/avatar-lizard.png"  alt="avatar">  
+      <img class="avatar" src="@/assets/avatar-lizard.png" alt="avatar">
       <div class="form-name">
         <label for="name" class="label-name">Name:</label>
         <input class="input-name" name="name" id="name" type="text">
       </div> 
-    </div>     
-    <div class="row-user-pass">
-      <div class="row-margin"></div> 
-      <div class="form-user-pass">
-        <label for="username" class="label-user-pass">Username:</label>
-        <input class="input-user-pass" name="username" id="username" type="text">
-      </div>
     </div>
-    <div class="row-user-pass">
-      <div class="row-margin"></div> 
-      <div class="form-user-pass">
-        <label for="password" class="label-user-pass">Password:</label>
-        <input class="input-user-pass" name="password" id="password" type="password">
-      </div>
+    <!-- 2nd row: username row -->
+    <div class="row-form-user">
+      <label for="username" class="label-user-pass">Username:</label>
+      <input class="input-user" name="username" id="username" type="text">
     </div>
+    <!-- 3rd row: password row -->
+    <div class="row-form-user">
+      <label for="password" class="label-user-pass">Password:</label>
+      <input class="input-user" name="password" id="password" type="password">
+    </div>
+    <!-- 4th row: buttons -->
     <div class="button-container"> 
       <!--TODO: handleClick to be implemented -->
-      <button class="button" @click="handleClick">Cancel</button>
-      <button class="button" @click="handleClick">Save</button>
+      <button class="btn" @click="handleClick">Cancel</button>
+      <button class="btn" @click="handleClick">Save</button>
     </div>  
   </div>
 </template>
  
  <script>
- export default {
-   name: 'UserSettings', }
+  // TODO: handleClick to be implemented
+  export default {
+    name: 'UserSettings'
+  }
  </script>
 
 <style scoped>
 
-.form-entries {                   
-  margin: auto;
-  margin-top: 16%;
-  width: 52%;
-}
+  /* Styling for the whole container */
+  .form-entries {
+    width: 50%;
+    margin: auto;
+    margin-top: 200px;
+  }
 
-.row-name {                        
-  display: flex;                  
-  gap: 15px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-
-.avatar {
-  width: 10%;
-  height: 10%;
-  min-width: 50px;
-  min-height: 50px;
-  border-radius: 33%;
-}
-
-.form-name {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-}
-
-.label-name {
-    font-family:Cantora One, Georgia;
-    color: #45C059;
-    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+  /* Styling for the name row */
+  .row-name {
     display: flex;
-}
+    gap: 15px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
 
-.input-name { 
-  font-family:Cantora One, Georgia;  
-  background-color:rgba(0, 0, 0, 0.301);
-  color: white;
-  padding: 12px 20px;
-  font-size: 16px;
-  border: 1px solid rgba(0, 0, 0, 0.301);
-  border-radius: 0;
-  box-sizing: border-box;
-}
-
-.row-user-pass {
-  display: flex;
-}
-
-.row-margin {                /* empty element, used for positioning the form-user-pass class */
-  width: 38%;
-  min-width: 50px;
-  min-height: 50px;
-  align-items: left;
-  justify-content: left;
-}
-
-.form-user-pass {
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  width: 100%; 
-  flex-grow: 3;
-  margin-top: 0.5%;
-  
-}
-
-.label-user-pass {
-    font-family:Cantora One, Georgia;
-    color: #45C059;
-    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+  /* Styling for the name form */
+  .form-name {
     display: flex;
-}
+    width: 50%; /* stretch the form to fill the container */
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+  }
 
-.input-user-pass { 
-  font-family:Cantora One, Georgia;  
-  background-color:rgba(0, 0, 0, 0.301);
-  color: white;
-  width: 62%;
-  padding: 12px 20px;
-  font-size: 16px;
-  border: 1px solid rgba(0, 0, 0, 0.301);
-  box-sizing: border-box;
-}
+  /* Styling for the avatar of the user in the first row */
+  .avatar {
+    width: 10%;
+    height: 10%;
+    min-width: 50px;
+    min-height: 50px;
+    border-radius: 33%;
+  }
 
-.button-container {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
+  /* Styling for the row of the username and password */
+  .row-form-user {
+    display: flex;
+    margin-top: 0.5%;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.button {
-  display: flex;
-  justify-content: center;
-  font-family:Cantora One, Georgia;
-  font-size: 100%;
-  color: black;
-  background-color: #45C059;
-  border: #45C059;
-  margin: 2%;
-}
+  /* Styling for the labels */
+  .label-name, .label-user-pass {
+    color: #45C059;
+    font-family: Cantora One, Georgia;
+    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+  }
+
+  /* Styling for the name label */
+  .label-user-pass {
+      width: 60%; /* the alternative to text-align: left */
+  }
+
+  /* Styling specific to the input fields */
+  .input-name, .input-user {
+    color: white;
+    font-size: 16px;
+    padding: 12px 20px;
+    box-sizing: border-box;
+    font-family: Cantora One, Georgia;
+    border: 1px solid rgba(0, 0, 0, 0.301);
+    background-color:rgba(0, 0, 0, 0.301);
+  }
+
+  /* Spcific styling for the name input field */
+  .input-user { width: 62%; }
+
+  /* Styling for the buttons container */
+  .button-container {
+    display: flex;
+    display: row;
+    gap: 30px;
+    justify-content: center;
+    padding-top: 2%;
+  }
+
+  /* Styling for the buttons */
+  .btn {
+    color: black;
+    font-size: 2.5vh;
+    border: #45C059;
+    background-color: #45C059;
+    font-family: Cantora One, Georgia;
+  }
 </style>
