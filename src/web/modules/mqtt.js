fa580f709506af8b3b7bb4b41178bc6a019e621f
gusbomjod <gusbomjod@student.gu.se>
2023-05-11 17:44:31 +0200

    #63 print reconnect status to console
    print to console if client is offline and when trying to recconect.

--- src/web/modules/mqtt.js
+++ src/web/modules/mqtt.js
@@ -1,29 +1,34 @@
-import mqtt from 'mqtt';                                      // import mqtt client library (install by using npm install mqtt --save)
+import mqtt from 'mqtt';                                        // import mqtt client library (install by using npm install mqtt --save)
 import { saveData } from './dataHandler.js'                   // import function that sensor data is passed to
 
 
-const options = {                                             // define the MQTT client and connect to the broker
-  host: '***.***.**.**',                                      // replace with the IP address of your MQTT broker
-  port: 1883                                                  // replace with the port number of your MQTT broker
+const options = {                                               // define the MQTT client and connect to the broker
+    host: '***.***.*.***',                                      // replace with the IP address of your MQTT broker
+    reconnectPeriod: 1000,                                      // default value for amount of ms between reconnect attempts
+    port: 1883                                                  // replace with the port number of your MQTT broker
 };
+const client = mqtt.connect(options);                           // connect to the mqtt broker
 
-const client = mqtt.connect(options);                         // connect to the mqtt broker
-
-export function connect() {                                   // subscribe to sensor topics and call function to handle incoming data
-    client.on('connect', function () {                        // runs once connected to MQTT broker
-      console.log('Connected to MQTT broker!');               // print connection message to console
-      client.subscribe('/terminarium/sensor/temperature');    // subscribe to all sensor topics
-      client.subscribe('/terminarium/sensor/humidity');
-      client.subscribe('/terminarium/sensor/vibration');
-      client.subscribe('/terminarium/sensor/moisture');
-      client.subscribe('/terminarium/sensor/light');
-      client.subscribe('/terminarium/sensor/loudness');
+export function connect() {                                     // subscribe to sensor topics and call function to handle incoming data
+    client.on('connect', function () {                          // runs once connected to MQTT broker
+        console.log('Connected to MQTT broker!');               // print connection message to console
+        client.subscribe('/terminarium/sensor/temperature');    // subscribe to all sensor topics
+        client.subscribe('/terminarium/sensor/humidity');
+        client.subscribe('/terminarium/sensor/vibration');
+        client.subscribe('/terminarium/sensor/moisture');
+        client.subscribe('/terminarium/sensor/light');
+        client.subscribe('/terminarium/sensor/loudness');
     });
-  
-    client.on('message', function (topic, message) {          // runs once a message is received 
-      console.log('Received message:', message.toString());   // print message received to console
-      saveData(topic, message.toString());                    // pass topic and sensor data as String to be saved
-    }); 
 
-      
-  }
+    client.on('message', function (topic, message) {            // runs once a message is received
+        console.log('Received message:', message.toString());   // print message received to console
+        saveData(topic, message.toString());                    // pass topic and sensor data as String to be saved
+    });
+    client.on('reconnect', function () {                        // runs if client is reconnecting
+        console.log('Trying to reconnect')
+    });
+    client.on('offline', function () {                          // runs if connection is lost
+        console.log('Client offline')
+    });
+
+}
