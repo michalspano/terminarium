02acf7349b51f148df92b65d2f283d1435e5d140
Michal Spano <spano@student.chalmers.se>
2023-05-12 10:16:16 +0000

    #82 Add Unit Test for DefaultPageLayout

--- src/web/terminarium-web/vite.config.js
+++ src/web/terminarium-web/vite.config.js
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // List plugins to use
   plugins: [vue()],
   resolve: {
     alias: {
+      // Define entry point for custom aliases
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }, 
+  test: {
+    /* For building web-based applications: 'jsdom'/'happy-dom';
+     * Reference: https://vitest.dev/config/#environment */
+    environment: 'happy-dom'
   }
 })
